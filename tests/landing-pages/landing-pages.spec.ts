2e22bb8467e94df030b4a5ba1fe5a49bae70c4c5
Iris Opper <iriszidon@gmail.com>
2025-12-27 15:11:01 +0200

    Rename page name

--- tests/landing-pages/landing-pages.spec.ts
+++ tests/landing-pages/landing-pages.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Landing pages tests", () => {
   test.only("open_some_link", async ({ page }) => {
-    const pageUnderTest = process.env.FORTRADE_LP
-      ? process.env.FORTRADE_LP
+    const pageUnderTest = process.env.FOTRADE_LP
+      ? process.env.FOTRADE_LP
       : "https://www.n12.co.il/";
     await page.goto(pageUnderTest);
   });
